7bf5d196055070d86c84cebe2bd242f4d9eb4517
apgyapgy <2648226617@qq.com>
2019-08-12 23:32:02 +0800

    enterpriseInfo
    项目搭建

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueWechatTitle from 'vue-wechat-title'
+
+
 Vue.config.productionTip = false
 
+
+Vue.use(VueWechatTitle)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Detail from '@/components/Detail/Detail'
+import Index from '@/components/Index/Index'
+import List from '@/components/List/List'
+import Login from '@/components/Login/Login'
+import Registry from '@/components/Registry/Registry'
 
 Vue.use(Router)
 
@@ -8,8 +13,37 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      meta:{
+      	title:'首页'
+      },
+      component: Index
+    },{
+      path: '/index',
+      name: 'Index',
+      component: Index
+    },{
+      path: '/detail',
+      name: 'Detail',
+      component: Detail
+    },{
+      path: '/list',
+      name: 'List',
+      component: List
+    },{
+      path: '/login',
+      name: 'Login',
+      meta:{
+      	title:'登录'
+      },
+      component: Login
+    },{
+      path: '/registry',
+      name: 'Registry',
+      meta:{
+      	title:'注册'
+      },
+      component: Registry
     }
   ]
 })
